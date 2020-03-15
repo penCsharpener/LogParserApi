f98d257f5e4c78b1fdd891ad31797de7fb5a291f
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-03-15 21:30:08 +0100

    setup repo, solution, projects

---
+++ tests/LogParserApi.ParserTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace LogParserApi.ParserTests {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
